643e59c838f6d9bb25e1272e3fe1f3a1b8d68c11
Robert Popovici <popovicirob01@gmail.com>
2025-03-15 17:46:43 +0200

    Add permission checks in the formatting

--- format.sh
+++ format.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-find . -type f -name "*.sh" -exec dos2unix {} +
+# Add execute permission on all shell scripts
+find . -type f -name "*.sh" -exec chmod +x {} + 2> /dev/null
+
+# Replace \r\n with \n for all shell scripts
+find . -type f -name "*.sh" -exec dos2unix {} + 2> /dev/null
